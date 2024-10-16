090a644a5b78b2eed22f532383566a0aeeba6163
bagas-rafa-hafiz <tuanmudarafa84@gmail.com>
2024-10-16 13:12:38 +0700

    Add files via upload

---
+++ TugasPercabangan/ganjilGenap.java
@@ -0,0 +1,19 @@
+package TugasPercabangan;
+import java.util.Scanner;
+public class ganjilGenap {
+    public static void main(String[] args) {
+        int num;
+        String jenis;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        num = sc.nextInt();
+        sc.close();
+        if(num % 2 == 0){
+            jenis = "Genap";
+        }else{
+            jenis = "Ganjil";
+        }
+        System.out.println("Bilangan nya adalah bilangan: "+jenis);    
+    }
+    
+}
